5cc73ba7e8dafe7f0d8dd61aec03bd4c032a6315
James Tognola <togdem1@googlemail.com>
2018-02-03 22:07:32 +0000

    Added repositories to DAL.

---
+++ MarkdownDocuments.DAL/Repositories/DocumentRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using MarkdownDocuments.Models.Models;
+
+namespace MarkdownDocuments.DAL.Repositories
+{
+    public class DocumentRepository : IRepository<DocumentModel>
+    {
+        public IEnumerable<DocumentModel> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DocumentModel Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DocumentModel Add(DocumentModel modal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DocumentModel Update(DocumentModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MarkdownDocuments.DAL/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarkdownDocuments.DAL.Repositories
+{
+    public interface IRepository<TModel>
+    {
+        IEnumerable<TModel> Get();
+        TModel Get(Guid id);
+        TModel Add(TModel modal);
+        TModel Update(TModel model);
+        void Delete(Guid id);
+    }
+}
