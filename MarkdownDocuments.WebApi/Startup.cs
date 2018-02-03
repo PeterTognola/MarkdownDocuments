59c4a7654efcf2eb0091c620ed16775a39bfb5a3
James Tognola <togdem1@googlemail.com>
2018-02-03 21:57:31 +0000

    Added mapping to webapi.

--- MarkdownDocuments.WebApi/Startup.cs
+++ MarkdownDocuments.WebApi/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MarkdownDocuments.Models.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,9 @@ namespace MarkdownDocuments.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Mapper Instantiation
+            services.AddTransient<IDocumentMapper, DocumentMapper>();
+            
             services.AddMvc();
         }
 
