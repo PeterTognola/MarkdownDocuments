e85c41a5c4913dbca91b12d7ab76a27fcbad1dcb
James Tognola <togdem1@googlemail.com>
2018-02-04 20:31:38 +0000

    Removed webapp, going to create standard react application.

--- MarkdownDocuments.WebApi/Program.cs
+++ MarkdownDocuments.WebApi/Program.cs
@@ -20,6 +20,7 @@ namespace MarkdownDocuments.WebApi
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:5001")
                 .Build();
     }
 }
