926f793f5c46be7e3562df254777e6bfda307b46
James Tognola <togdem1@googlemail.com>
2018-02-11 21:51:01 +0000

    Updated style of show document componenet.

--- MarkdownDocuments.App/web/src/components/document/Show.js
+++ MarkdownDocuments.App/web/src/components/document/Show.js
@@ -7,104 +7,81 @@ import { del, loading, error } from '../../actions/document/delete';
 import {templates} from "../../utils/templates";
 
 class Show extends Component {
-  static propTypes = {
-    error: PropTypes.string,
-    loading: PropTypes.bool.isRequired,
-    retrieved: PropTypes.object,
-    retrieve: PropTypes.func.isRequired,
-    reset: PropTypes.func.isRequired,
-    deleteError: PropTypes.string,
-    deleteLoading: PropTypes.bool.isRequired,
-    deleted: PropTypes.object,
-    del: PropTypes.func.isRequired
-  };
+    static propTypes = {
+        error: PropTypes.string,
+        loading: PropTypes.bool.isRequired,
+        retrieved: PropTypes.object,
+        retrieve: PropTypes.func.isRequired,
+        reset: PropTypes.func.isRequired,
+        deleteError: PropTypes.string,
+        deleteLoading: PropTypes.bool.isRequired,
+        deleted: PropTypes.object,
+        del: PropTypes.func.isRequired
+    };
 
-  componentDidMount() {
-    this.props.retrieve(decodeURIComponent(this.props.match.params.id));
-  }
+    componentDidMount() {
+        this.props.retrieve(decodeURIComponent(this.props.match.params.id));
+    }
 
-  componentWillUnmount() {
-    this.props.reset();
-  }
+    componentWillUnmount() {
+        this.props.reset();
+    }
 
-  del = () => {
-    if (window.confirm('Are you sure you want to delete this item?')) this.props.del(this.props.retrieved);
-  };
+    del = () => {
+        if (window.confirm('Are you sure you want to delete this item?')) this.props.del(this.props.retrieved);
+    };
 
-  render() {
-    if (this.props.deleted) return <Redirect to=".."/>;
+    render() {
+        if (this.props.deleted) return <Redirect to=".."/>;
 
-    let item = this.props.retrieved;
+        let item = this.props.retrieved;
 
-    if (item !== null) item = this.props.retrieved.value;
+        if (item !== null) item = this.props.retrieved.value;
 
-    return (<div>
-      <h1>Show {item && item['id']}</h1>
+        return (
+            <div>
+                {this.props.loading && templates.loading()}
+                {this.props.error && <div className="alert alert-danger" role="alert"><span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> {this.props.error}</div>}
+                {this.props.deleteError && <div className="alert alert-danger" role="alert"><span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> {this.props.deleteError}</div>}
 
-      {this.props.loading && templates.loading()}
-      {this.props.error && <div className="alert alert-danger" role="alert"><span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> {this.props.error}</div>}
-      {this.props.deleteError && <div className="alert alert-danger" role="alert"><span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> {this.props.deleteError}</div>}
-
-      {item && <div className="table-responsive">
-        <table className="table table-striped table-hover">
-          <thead>
-            <tr>
-              <th>Field</th>
-              <th>Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>id</td>
-              <td>{item['id']}</td>
-            </tr>
-            <tr>
-              <td>title</td>
-              <td>{item['title']}</td>
-            </tr>
-            <tr>
-              <td>body</td>
-              <td>{item['body']}</td>
-            </tr>
-            <tr>
-              <td>creationDate</td>
-              <td>{item['creationDate']}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      }
-      <Link to=".." className="btn btn-default">Back to list</Link>
-      {item && <Link to={`/document/edit/${encodeURIComponent(item['id'])}`}>
-        <button className="btn btn-warning">Edit</button>
-        </Link>
-      }
-      <button onClick={this.del} className="btn btn-danger">Delete</button>
-    </div>);
-  }
+                {item &&
+                    <div className="page">
+                        <h1>{item['title']}</h1>
+                        <p>{item['body']}</p>
+                    </div>
+                }
+                <Link to=".." className="btn btn-default">Back to list</Link>
+                {item && <Link to={`/document/edit/${encodeURIComponent(item['id'])}`}>
+                <button className="btn btn-warning">Edit</button>
+                </Link>
+                }
+                <button onClick={this.del} className="btn btn-danger">Delete</button>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  return {
-    error: state.documentReducers.show.error,
-    loading: state.documentReducers.show.loading,
-    retrieved:state.documentReducers.show.retrieved,
-    deleteError: state.documentReducers.del.error,
-    deleteLoading: state.documentReducers.del.loading,
-    deleted: state.documentReducers.del.deleted,
-  };
+    return {
+        error: state.documentReducers.show.error,
+        loading: state.documentReducers.show.loading,
+        retrieved:state.documentReducers.show.retrieved,
+        deleteError: state.documentReducers.del.error,
+        deleteLoading: state.documentReducers.del.loading,
+        deleted: state.documentReducers.del.deleted
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    retrieve: id => dispatch(retrieve(id)),
-    del: item => dispatch(del(item.value)),
-    reset: () => {
-      dispatch(reset());
-      dispatch(error(null));
-      dispatch(loading(false));
-    },
-  }
+    return {
+        retrieve: id => dispatch(retrieve(id)),
+        del: item => dispatch(del(item.value)),
+        reset: () => {
+            dispatch(reset());
+            dispatch(error(null));
+            dispatch(loading(false));
+        }
+    }
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Show);
