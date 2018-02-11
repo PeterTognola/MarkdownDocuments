2747d9979d72465ae45201678be32255f96da530
James Tognola <togdem1@googlemail.com>
2018-02-11 20:29:49 +0000

    Fixing styles for document component transitions.

--- MarkdownDocuments.App/web/src/utils/templates.js
+++ MarkdownDocuments.App/web/src/utils/templates.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export class templates {
     static loading() {
-        return <div style={{position:"fixed", top:"50%", left:"50%", transform:"translate(-50%, -50%)", width:"40px", height:"40px" }} className="loading">
+        return <div style={{position:"fixed", top:"50%", left:"50%", transform:"translate(-50%, -50%)", width:"40px", height:"40px", zIndex:"99" }} className="loading">
             <span className="icon loading"></span>
         </div>;
     }
