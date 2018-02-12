356ab93b0d519c79fd19740baffe8cbafddd161e
James Tognola <togdem1@googlemail.com>
2018-02-12 21:12:09 +0000

    Fixed jest/enzyme crap. Problem known, https://github.com/facebook/jest/issues/4419, caused by yarn and npm being in the same place...

--- MarkdownDocuments.App/web/src/index.test.js
+++ MarkdownDocuments.App/web/src/index.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { shallow } from 'enzyme'; // todo add enzyme package.
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-    shallow(App.body());
+    Enzyme.shallow(App.body());
 });
+
