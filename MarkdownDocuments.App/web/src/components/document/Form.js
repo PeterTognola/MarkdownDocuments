d14abd96f28f3619a3a61aada7f28cf3cbb2372c
James Tognola <togdem1@googlemail.com>
2018-02-13 21:44:48 +0000

    Fixed update.

--- MarkdownDocuments.App/web/src/components/document/Form.js
+++ MarkdownDocuments.App/web/src/components/document/Form.js
@@ -13,31 +13,36 @@ class Editor extends Component {
     render() {
         return (
             <div className="app">
-                <textarea className="editor" id="document_body" defaultValue="test" onChange={this.onChange} />
-
+                <Field component={renderField} defaultValue="test" name="body" type="textarea" placeholder="The title..." required={true} onChange={this.onChange} />
                 <Markdown className="preview" source={this.state ? this.state.source : "test"} escapeHtml />
             </div>
         );
     }
 }
 
-class Form extends Component {
-    renderField(data) {
-        const hasError = data.meta.touched && !!data.meta.error;
-        if (hasError) {
-            data.input['aria-describedby'] = `document_${data.input.name}_helpBlock`;
-            data.input['aria-invalid'] = true;
-        }
 
-        return (
-            <div className={`form-group${hasError ? ' has-error' : ''}`}>
-                <label htmlFor={`document_${data.input.name}`} className="control-label">{data.input.name}</label>
-                <input {...data.input} type={data.type} step={data.step} required={data.required} placeholder={data.placeholder} id={`document_${data.input.name}`} className="form-control"/>
-                {hasError && <span className="help-block" id={`document_${data.input.name}_helpBlock`}>{data.meta.error}</span>}
-            </div>
-        );
+const renderField = (data) => {
+    const hasError = data.meta.touched && !!data.meta.error;
+    if (hasError) {
+        data.input['aria-describedby'] = `document_${data.input.name}_helpBlock`;
+        data.input['aria-invalid'] = true;
     }
 
+    return (
+        <div className={`form-group${hasError ? ' has-error' : ''}`}>
+            <label htmlFor={`document_${data.input.name}`} className="control-label">{data.input.name}</label>
+            {
+                data.type === "textarea"
+                    ? <textarea {...data.input} id={`document_${data.input.name}`} required={data.required} placeholder={data.placeholder} step={data.step} />
+                    : <input {...data.input} type={data.type} step={data.step} required={data.required} placeholder={data.placeholder} id={`document_${data.input.name}`} />
+            }
+
+            {hasError && <span className="help-block" id={`document_${data.input.name}_helpBlock`}>{data.meta.error}</span>}
+        </div>
+    );
+}
+
+class Form extends Component {
     // This form requires a bit of a hack.
     // The form needs to be submitted externally, using id "formEditor"
     // todo try not to use a ol'hack...
@@ -47,9 +52,8 @@ class Form extends Component {
         return (
             <div className="page">
                 <form onSubmit={handleSubmit} id="formEditor">
-                    <Field component={this.renderField} name="title" type="text" placeholder="The title..." required={true} />
+                    <Field component={renderField} name="title" type="text" placeholder="The title..." required={true} />
                     <Editor />
-                    {/*<Field component={this.renderField} type="text" placeholder="The body..." required={true}/>*/}
                 </form>
             </div>
         );
