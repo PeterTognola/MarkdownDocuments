5317f5594b00c905c7447a619805ba1ad2b1a783
James Tognola <togdem1@googlemail.com>
2018-02-17 20:25:11 +0000

    Added postcss...

--- MarkdownDocuments.App/web/src/App.js
+++ MarkdownDocuments.App/web/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { reducer as form } from 'redux-form';
-import { Link, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import { syncHistoryWithStore, routerReducer as routing } from 'react-router-redux'
 import './index.css';
@@ -31,11 +31,7 @@ export default class App {
             <ThemeProvider theme={Theme}>
                 <div className="container">
                     <Route render={() =>
-                        <ul className="menu">
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/document">Documents</Link></li>
-                            <li><Link to="/">Account</Link></li>
-                        </ul>
+                        <div></div>
                     }/>
 
                     {documentRoutes}
