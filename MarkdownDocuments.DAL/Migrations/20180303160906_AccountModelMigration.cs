832a029af7144338f93e29d79b5f3cfe1001c7fe
James Tognola <togdem1@googlemail.com>
2018-03-03 16:09:37 +0000

    Moved to AccountModel instead of MarkdownUser.

---
+++ MarkdownDocuments.DAL/Migrations/20180303160906_AccountModelMigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MarkdownDocuments.DAL.Migrations
+{
+    public partial class AccountModelMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
