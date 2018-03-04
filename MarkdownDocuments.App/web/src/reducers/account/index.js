8a3be2549089abd80bcce2a8c08a1811d5d18ff6
James Tognola <togdem1@googlemail.com>
2018-03-04 19:48:49 +0000

    Removed sprite.

--- MarkdownDocuments.App/web/src/reducers/account/index.js
+++ MarkdownDocuments.App/web/src/reducers/account/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 import create from "./create";
 import update from "./update";
-import show from "./show";
 import login from "./login";
 
-export default combineReducers({ create, update, show, login });
+export default combineReducers({ create, update, login });
